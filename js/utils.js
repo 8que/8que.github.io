7ea46ee6104e2612a2f2b14d421ff1a0a3b367af
8que <3134746115@qq.com>
2023-07-26 15:14:21 +0800

    Site updated: 2023-07-26 15:14:21

--- js/utils.js
+++ js/utils.js
@@ -311,12 +311,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！';
+        document.title = '(^～^) 我在看着你哦';
         clearTimeout(titleTime);
     }
     else {
         //返回当前页面时标签显示内容
-        document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
+        document.title = '哎呀，被发现惹(⋟﹏⋞)' + OriginTitile;
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
